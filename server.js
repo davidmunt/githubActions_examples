ac1f28cb1bf80f95fa79cf0f8d999c24dc10a447
David <davidmunteanonti@gmail.com>
2025-10-15 21:13:00 +0200

    Añado console.log

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-const express = require("express")
+const express = require("express");
 const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
   res.send("Hola GitHub Actions!");
 });
+
+console.log("Deberia fallar aqui");
 
 app.listen(PORT, () => {
   console.log(`Servidor en ejecución en http://localhost:${PORT}`);
