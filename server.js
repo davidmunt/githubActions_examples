947351de255b48c62abbe9aeaf2560130e2084df
David <davidmunteanonti@gmail.com>
2025-10-15 21:29:47 +0200

    Ahora deberia de funcionar

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hola GitHub Actions!");
+app.get('/', (req, res) => {
+  res.send('Hola GitHub Actions!');
 });
 
 app.listen(PORT, () => {});
